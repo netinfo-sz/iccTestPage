ae4e6fab8c7cf331ec2c35fa187cd3134dbb939f
chenpeng <chenpeng@netinfosz.com>
2025-01-20 10:18:19 +0800

    语音点呼参数优化

--- docs/components/interface/voice/dialVoice.vue
+++ docs/components/interface/voice/dialVoice.vue
@@ -1,10 +1,15 @@
 <script setup name="DialVoice">
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 import {ElMessage} from 'element-plus'
 import { checkInstance, getInstanceFCC } from '../../use-fcc'
 import { setLog } from '../../use-log'
 const token = ref('')
 const userAccount = ref('')
+const location = reactive({
+  locX: '',
+  locY: '',
+  scale: 1
+})
 const loading = ref(false)
 const dialVoice = async () => {
   setLog({
@@ -20,10 +25,7 @@ const dialVoice = async () => {
   if (!await checkInstance()) {
     setLog({
       name: '语音呼叫',
-      msg: '请先实例化'
-    })
-    ElMessage({
-      message: '请先实例化',
+      msg: '请先实例化',
       type: 'warning'
     })
     return
@@ -31,17 +33,15 @@ const dialVoice = async () => {
   if (!userAccount.value) {
     setLog({
       name: '语音呼叫',
-      msg: `参数错误,请输入呼叫账号`
-    })
-    ElMessage({
-      message: '请输入呼叫账号',
+      msg: `参数错误,请输入呼叫账号`,
       type: 'warning'
     })
     return
   }
   loading.value = true
   let result = await getInstanceFCC().dialVoice({
-    userAccount: userAccount.value
+    userAccount: userAccount.value,
+    location: location
   })
   setLog({
     name: '语音呼叫结果',
@@ -65,7 +65,16 @@ const dialVoice = async () => {
 <template>
 <el-form>
   <el-form-item label="账号">
-    <el-input v-model="userAccount" placeholder="请输入测试账号"></el-input>
+    <el-input v-model="userAccount" placeholder="请输入对方账号"></el-input>
+  </el-form-item>
+  <el-form-item label="locX">
+    <el-input v-model="location.locX" placeholder="请输入X轴位置"></el-input>
+  </el-form-item>
+  <el-form-item label="locY">
+    <el-input v-model="location.locY" placeholder="请输入Y轴位置"></el-input>
+  </el-form-item>
+  <el-form-item label="scale">
+    <el-input v-model="location.scale" placeholder="请输入缩放比率"></el-input>
   </el-form-item>
   <el-form-item>
     <el-button type="primary" @click="dialVoice" :loading="loading">发起语音点呼</el-button>
