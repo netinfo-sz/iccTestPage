79afcf0e85716920d6917611ed5b76a9db423a06
oumoyong <84561814@qq.com>
2025-02-11 11:01:05 +0800

    打开矩阵监控

--- docs/components/interface/monitor/enterMatrix.vue
+++ docs/components/interface/monitor/enterMatrix.vue
@@ -3,10 +3,9 @@ import { ref } from 'vue'
 import {ElMessage} from 'element-plus'
 import { checkInstance, getInstanceFCC } from '../../use-fcc'
 import { setLog } from '../../use-log'
+import matrixDeviceList from './matrixList.vue'
 const loading = ref(false)
-const showDialog = ref(false)
-const divceList = ref(null)
-const enterMatrixList = ref([{
+const matrixList = ref([{
   deviceCode: "08594172951682810101#f549ae3dca014060b2b7cf3d17d03b32",
   deviceId: "912910",
   deviceName: "办公A区西北"
@@ -15,10 +14,6 @@ const enterMatrixList = ref([{
   deviceId: "912909",
   deviceName: "走廊西-东向"
 }])
-const enterMatrix = () => {
-  showDialog.value = true
-  divceList.value = JSON.stringify(enterMatrixList.value)
-}
 const confirmEnterMatrix = async () => {
   setLog({
     name: '开始打开矩阵监控'
@@ -42,7 +37,7 @@ const confirmEnterMatrix = async () => {
     return
   }
   loading.vue = true
-  let result = await getInstanceFCC().enterMatrix(JSON.parse(divceList.value))
+  let result = await getInstanceFCC().enterMatrix(matrixList.value)
   setLog({
     name: '打开矩阵监控结果',
     msg: result
@@ -79,19 +74,24 @@ const closeMatrix = async () => {
     })
   }
 }
+const addMeeting = () => {
+  matrixList.value.push({
+    deviceCode: '',
+    deviceId: '',
+    deviceName: ''
+  })
+}
+const delDivice = (index) => {
+  matrixList.value.splice(index, 1)
+}
 </script>
 
 <template>
 <el-form>
+  <matrixDeviceList :matrixList="matrixList" @add-divce="addMeeting" @del-divice="delDivice"></matrixDeviceList>
   <el-form-item>
-    <el-button type="primary" @click="enterMatrix" v-loading="loading">打开矩阵监控</el-button>
+    <el-button type="primary" @click="confirmEnterMatrix" >打开矩阵监控</el-button>
     <el-button type="primary" @click="closeMatrix" v-loading="loading">关闭矩阵监控</el-button>
   </el-form-item>
 </el-form>
-<el-dialog title="监控设备数据" v-model="showDialog" width="36%" center>
-  <el-input v-model="divceList" type="textarea" :rows="5"></el-input>
-  <template #footer>
-    <el-button type="primary" @click="confirmEnterMatrix">确定</el-button>
-  </template>
-</el-dialog>
 </template>
---
+++ docs/components/interface/monitor/matrixList.vue
@@ -0,0 +1,78 @@
+<script lang="ts" setup name="addMatrix">
+const emit = defineEmits<{
+  (e: 'add-divce'): void;
+  (e: 'del-divice', index: number): void; // 新增事件定义
+}>();
+
+const props = defineProps<{
+  matrixList: Array<any>
+}>()
+const addMeeting = () => {
+  emit('add-divce');
+};
+const delDivice = (index: number) => {
+  emit('del-divice', index); // 触发新事件
+};
+
+</script>
+
+<template>
+<el-form>
+  <el-form-item :style="{'margin-top': '10px'}">
+      <el-button type="primary" @click="addMeeting">添加监控设备</el-button>
+  </el-form-item>
+  <div class="list-head">
+    <span class="span-name">设备名称</span>
+    <span class="span-code">设备编号</span>
+    <span class="span-id">设备ID</span>
+  </div>
+  <el-form-item :style="{'margin-top': '10px'}" v-for="(item, index) in props.matrixList" :key="index" class="meet-list">
+    <el-input class='divice-name' v-model="item.deviceName" placeholder="设备名称"></el-input>
+    <el-input class='divice-code' v-model="item.deviceCode" placeholder="设备编号"></el-input>
+    <el-input class='divice-id' v-model="item.deviceId" placeholder="设备ID"></el-input>
+    <el-button v-show="index > 1" type="danger" @click="delDivice(index)">删除</el-button>
+  </el-form-item>
+</el-form>
+</template>
+
+<style scoped>
+.list-head{
+  display: flex;
+  align-items: center;
+}
+.span-name{
+  width: 20%;
+  margin-right: 1%;
+}
+.span-code{
+  width: 44%;
+  margin-right: 1%;
+}
+.span-id{
+  width: 20%
+}
+.list-head span:last-child{
+  margin-right: 0;
+}
+.meet-list{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+}
+.divice-name {
+  width: 20%;
+  margin-right: 1%;
+}
+.divice-code {
+  width: 44%;
+  margin-right: 1%;
+}
+.divice-id {
+  width: 20%;
+  margin-right: 1%;
+}
+.meet-item:last-child{
+  margin-right: 0;
+}
+</style>
