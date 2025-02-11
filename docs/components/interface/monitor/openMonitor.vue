289cf8e7e464aa68f00b7f285b321ade635b62eb
oumoyong <84561814@qq.com>
2025-02-11 15:10:38 +0800

    打开单个监控

--- docs/components/interface/monitor/openMonitor.vue
+++ docs/components/interface/monitor/openMonitor.vue
@@ -4,8 +4,7 @@ import {ElMessage} from 'element-plus'
 import { checkInstance, getInstanceFCC } from '../../use-fcc'
 import { setLog } from '../../use-log'
 const loading = ref(false)
-const showDialog = ref(false)
-const formData = reactive({
+const deviceInfo = reactive({
   resId: '08594172957097100101#f549ae3dca014060b2b7cf3d17d03b32',
   name: '设备一',
   x: '480',
@@ -13,9 +12,6 @@ const formData = reactive({
   width: 640,
   height: 640
 })
-const openMonitor  = async () => {
-  showDialog.value = true
-}
 // 确认打开监控
 const confirmOpenMonitor= async () => {
   setLog({
@@ -40,8 +36,7 @@ const confirmOpenMonitor= async () => {
     return
   }
   loading.vue = true
-  showDialog.value = false
-  let result = await getInstanceFCC().openMonitor(formData)
+  let result = await getInstanceFCC().openMonitor(deviceInfo)
   setLog({
     name: '打开监控结果',
     msg: result
@@ -63,7 +58,7 @@ const confirmOpenMonitor= async () => {
 const offMonitoring = async () => { 
   let result = await getInstanceFCC().offMonitoring({
     winId: '0000', // 0000”在主窗口关闭监控， “0301”在矩阵中关闭监控, “0401”在场所中关闭监控 以此类推, 不传默认为”0000”
-    resId: formData.resId // 设备编码
+    resId: deviceInfo.resId // 设备编码
   })
   setLog({
     name: '关闭监控结果',
@@ -85,35 +80,52 @@ const offMonitoring = async () => {
 </script>
 
 <template>
-<el-form>
+<el-form :model="deviceInfo">
+  <div>监控设备信息</div>
+  <div>
+    <el-row>
+      <el-col :span="7">
+        <el-form-item label="X轴偏移量:">
+          <el-input v-model="deviceInfo.x" placeholder="X轴偏移量"></el-input>
+        </el-form-item>
+      </el-col>
+      <el-col :span="7">
+        <el-form-item label="Y轴偏移量:">
+          <el-input v-model="deviceInfo.y" placeholder="Y轴偏移量"></el-input>
+        </el-form-item>
+      </el-col>
+      <el-col :span="7">
+        <el-form-item label="宽度:">
+          <el-input v-model.number="deviceInfo.width" placeholder="宽度"></el-input>
+        </el-form-item>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="7">
+        <el-form-item label="高度:">
+          <el-input v-model.number="deviceInfo.height" placeholder="高度"></el-input>
+        </el-form-item>
+      </el-col>
+      <el-col :span="7">
+        <el-form-item label="设备编码:">
+          <el-input v-model="deviceInfo.resId" placeholder="设备编码"></el-input>
+        </el-form-item>
+      </el-col>
+      <el-col :span="7">
+        <el-form-item label="设备名称:">
+          <el-input v-model="deviceInfo.name" placeholder="设备名称"></el-input>
+        </el-form-item>
+      </el-col>
+    </el-row>
+  </div>
   <el-form-item>
-    <el-button type="primary" @click="openMonitor" v-loading="loading">打开监控</el-button>
+    <el-button type="primary" @click="confirmOpenMonitor" v-loading="loading">打开监控</el-button>
     <el-button type="primary" @click="offMonitoring">关闭监控</el-button>
   </el-form-item>
 </el-form>
-<el-dialog title="查询会议列表数据" v-model="showDialog" width="36%" center>
-  <el-form :model="formData">
-    <el-form-item label="设备编码">
-      <el-input v-model="formData.resId" placeholder="请输入设备编码"></el-input>
-    </el-form-item>
-    <el-form-item label="设备名称">
-      <el-input v-model="formData.name" placeholder="请输入设备名称"></el-input>
-    </el-form-item>
-    <el-form-item label="X轴坐标">
-      <el-input v-model="formData.x" placeholder="请输入X轴坐标"></el-input>
-    </el-form-item>
-    <el-form-item label="Y轴坐标">
-      <el-input v-model="formData.y" placeholder="请输入Y轴坐标"></el-input>
-    </el-form-item>
-    <el-form-item label="宽度">
-      <el-input v-model="formData.width" placeholder="请输入宽度"></el-input>
-    </el-form-item>
-    <el-form-item label="高度">
-      <el-input v-model="formData.height" placeholder="请输入高度"></el-input>
-    </el-form-item>
-  </el-form>
-  <template #footer>
-    <el-button type="primary" @click="confirmOpenMonitor" v-loading="loading">确定</el-button>
-  </template>
-</el-dialog>
 </template>
+<style scoped>
+.el-col {
+  margin-right: 16px;
+}
+</style>
